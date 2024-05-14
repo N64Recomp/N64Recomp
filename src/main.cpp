be29c979c8593367f2dcafc5d33fe4f4c7928199
angie <angheloalf95@gmail.com>
2024-05-14 15:53:28 -0400

    Recomp __muldi3 and __umoddi3

--- src/main.cpp
+++ src/main.cpp
@@ -500,14 +500,12 @@ std::unordered_set<std::string> ignored_funcs {
     "rmonGetRcpRegister",
     "kdebugserver",
     "send",
+
     // libgcc math routines (these throw off the recompiler)
-    "__muldi3",
     "__divdi3",
     "__udivdi3",
-    "__umoddi3",
-    "div64_64",
-    "div64_32",
     "__moddi3",
+
     // ido math routines
     "__ll_div",
     "__ll_lshift",
@@ -626,6 +624,11 @@ std::unordered_set<std::string> renamed_funcs{
     "clear_bufs",
     "fill_inbuf",
     "flush_window",
+
+    "__muldi3",
+    "__umoddi3",
+    "div64_64",
+    "div64_32",
 };
 
 bool read_symbols(RecompPort::Context& context, const ELFIO::elfio& elf_file, ELFIO::section* symtab_section, uint32_t entrypoint, bool has_entrypoint, bool use_absolute_symbols) {
