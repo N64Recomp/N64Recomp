391511c79543bea25eabbb32e5f4fb461049af44
Mr-Wiseguy <mrwiseguyromhacking@gmail.com>
2024-10-03 01:56:15 -0400

    Remove luajit backend

--- include/generator.h
+++ include/generator.h
@@ -98,46 +98,6 @@ namespace N64Recomp {
         void get_notation(BinaryOpType op_type, std::string& func_string, std::string& infix_string) const;
         std::ostream& output_file;
     };
-
-    class LuajitGenerator final : Generator {
-    public:
-        LuajitGenerator(std::ostream& output_file) : output_file(output_file) {};
-        void process_binary_op(const BinaryOp& op, const InstructionContext& ctx) const final;
-        void process_unary_op(const UnaryOp& op, const InstructionContext& ctx) const final;
-        void process_store_op(const StoreOp& op, const InstructionContext& ctx) const final;
-        void emit_function_start(const std::string& function_name) const final;
-        void emit_function_end() const final;
-        void emit_function_call_lookup(uint32_t addr) const final;
-        void emit_function_call_by_register(int reg) const final;
-        void emit_function_call_by_name(const std::string& func_name) const final;
-        void emit_goto(const std::string& target) const final;
-        void emit_label(const std::string& label_name) const final;
-        void emit_variable_declaration(const std::string& var_name, int reg) const final;
-        void emit_branch_condition(const ConditionalBranchOp& op, const InstructionContext& ctx) const final;
-        void emit_branch_close() const final;
-        void emit_switch(const std::string& jump_variable, int shift_amount) const final;
-        void emit_case(int case_index, const std::string& target_label) const final;
-        void emit_switch_error(uint32_t instr_vram, uint32_t jtbl_vram) const final;
-        void emit_switch_close() const final;
-        void emit_return() const final;
-        void emit_check_fr(int fpr) const final;
-        void emit_check_nan(int fpr, bool is_double) const final;
-        void emit_cop0_status_read(int reg) const final;
-        void emit_cop0_status_write(int reg) const final;
-        void emit_cop1_cs_read(int reg) const final;
-        void emit_cop1_cs_write(int reg) const final;
-        void emit_muldiv(InstrId instr_id, int reg1, int reg2) const final;
-        void emit_syscall(uint32_t instr_vram) const final;
-        void emit_do_break(uint32_t instr_vram) const final;
-        void emit_pause_self() const final;
-        void emit_trigger_event(size_t event_index) const final;
-        void emit_comment(const std::string& comment) const final;
-    private:
-        void get_operand_string(Operand operand, UnaryOpType operation, const InstructionContext& context, std::string& operand_string) const;
-        void get_binary_expr_string(BinaryOpType type, const BinaryOperands& operands, const InstructionContext& ctx, const std::string& output, std::string& expr_string) const;
-        void get_notation(BinaryOpType op_type, std::string& func_string, std::string& infix_string) const;
-        std::ostream& output_file;
-    };
 }
 
 #endif
